99b06ccbd00dca7d171ede4cccb3bb2a7e1591a4
user <250090@newuu.uz>
2026-04-14 21:44:44 +0500

    yeah

--- src/BookReservation.java
+++ src/BookReservation.java
@@ -1,2 +1,27 @@
-public class BookReservation {
+import java.util.Date;
+
+class BookReservation {
+    Date creationDate;
+    ReservationStatus status;
+    Member member;
+    BookItem book;
+
+    public BookReservation(Member member, BookItem book) {
+        this.creationDate = new Date();
+        this.status = ReservationStatus.WAITING;
+        this.member = member;
+        this.book = book;
+    }
+
+    public void completeReservation() {
+        this.status = ReservationStatus.COMPLETED;
+    }
+
+    public void cancelReservation() {
+        this.status = ReservationStatus.CANCELED;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
 }
