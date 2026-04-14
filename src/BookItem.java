4b68221596f9777f736a0eb76efea696c5f741d8
user <250090@newuu.uz>
2026-04-14 21:45:49 +0500

    ..

--- src/BookItem.java
+++ src/BookItem.java
@@ -1,2 +1,19 @@
-public class BookItem extends Book {
-}
+import java.util.Date;
+
+class BookItem extends Book {
+    String barcode;
+    boolean isReferenceOnly;
+    Date borrowedDate;
+    Date dueDate;
+    double price;
+    BookFormat format;
+    BookStatus status;
+    Date dateOfPurchase;
+    Date publicationDate;
+
+    public boolean checkout(Member member) {
+        if (status != BookStatus.AVAILABLE) return false;
+
+        status = BookStatus.LOANED;
+        borrowedDate = new Date();
+        dueDate = new Date(System.currentTimeMillis() + 7L*24*60*60*1000);
