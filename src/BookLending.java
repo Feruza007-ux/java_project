4954622ebc034299df5ede8e8bd3ae9267f338ff
user <250090@newuu.uz>
2026-04-14 21:45:04 +0500

    ..

--- src/BookLending.java
+++ src/BookLending.java
@@ -1,2 +1,21 @@
-public class BookLending {
-}
+import java.util.Date;
+
+class BookLending {
+    Date creationDate;
+    Date dueDate;
+    Date returnDate;
+    BookItem book;
+    Member member;
+
+    public BookLending(BookItem book, Member member) {
+        this.creationDate = new Date();
+        this.dueDate = new Date(System.currentTimeMillis() + 7L * 24 * 60 * 60 * 1000); // 7 days
+        this.book = book;
+        this.member = member;
+    }
+
+    public void returnBook() {
+        this.returnDate = new Date();
+        book.status = BookStatus.AVAILABLE;
+       // member.borrowedBooks.remove(book);
+    }
