7cb39512a2504865f470984f9e71e1000c21e723
Durdona <odiljonovad3@gmail.com>
2026-04-20 13:40:04 +0500

    Define Book class with detailed properties and constructor

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,21 @@
 public class Book {
+    private String ISBN;
+    private String title;
+    private String subject;
+    private String publisher;
+    private String language;
+    private int numberOfPages;
+
+    public Book(String ISBN, String title, String subject, String publisher, String language, int numberOfPages){
+        this.ISBN=ISBN;
+        this.title=title;
+        this.subject=subject;
+        this.publisher=publisher;
+        this.language=language;
+        this.numberOfPages=numberOfPages;
+    }
+
+    public String getTitle(){
+        return title;
+    }
 }
